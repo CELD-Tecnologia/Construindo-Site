19d1050948f76dcd4941873bf52911fd0665712e
CELD-Tecnologia <105760503+CELD-Tecnologia@users.noreply.github.com>
2022-06-01 19:23:24 -0300

    CONSTRUINDO SITE - Ajuste de páginas

--- _painelAdm/_edicao/_conteudo/_php/excluirBanner.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!isset($_SESSION)) { session_start(); }
-include_once("../../../_php/conexao.php"); 
-
-$idImagem=$_GET['idImagem'];
-$sql = mysqli_query($conn, "DELETE from tbgaleria WHERE idImagem = $idImagem AND idSetorImagem = 1 AND idSite = " . $_SESSION['idSite']);
-
-echo '<meta http-equiv="refresh" content="0;url=../banner.php">';
-?>
