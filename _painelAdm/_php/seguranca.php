2e079afbd3fbbd303076033b2b4e264a8679a57c
LucasDourado94 <75843511+LucasDourado94@users.noreply.github.com>
2023-03-16 20:01:23 -0300

    TROCA DO NOME DO PAINEL ADM

--- _painelAdm/_php/seguranca.php
+++
@@ -1,9 +0,0 @@
-<?
-    if(!isset($_SESSION)) { session_start(); }
-
-	if($_SESSION['cd_usuario'] == 0) { 
-        echo '<meta http-equiv="refresh" content="0;url=http://construindosite.com.br">';
-    } else {
-        echo $_SESSION['cd_usuario'];
-    }
-?>
--- _painelAdm/logoff.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(isset($_SESSION)) {
-        session_destroy();
-    }
-
-    echo '<meta http-equiv="refresh" content="0;url=../">';
-?>
